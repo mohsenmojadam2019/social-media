c2339f7b8c15bb89c9374ba3a01a66567dc4f418
gech11 <getachewfikadu77@gmail.com>
2021-04-23 17:45:33 +0300

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,7 @@ use Illuminate\Support\Str;
 
 Route::get('/', function () {
     return view('welcome');
-});
+});Auth::routes();
+
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
