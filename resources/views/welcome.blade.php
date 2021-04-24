fa8daaa8460dcb757d589c855b77a64264d08259
gech11 <getachewfikadu77@gmail.com>
2021-04-24 17:25:08 +0300

    initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,10 +31,10 @@
     <div class="min-h-screen flex items-center justify-center">
         <div class="flex flex-col justify-around h-full">
             <div>
-                <h1 class="mb-6 text-yellow-900 text-center font-light tracking-wider text-4xl sm:mb-8 sm:text-6xl">
+                <h1 class="mb-6 text-blue-500 text-center font-light tracking-wider text-4xl sm:mb-8 sm:text-6xl">
                     {{ config('app.name', 'Laravel') }}
                 </h1>
-                <ul class="flex flex-col space-y-2 sm:flex-row sm:flex-wrap sm:space-x-8 sm:space-y-0">
+                <ul class="text-blue-500 flex flex-col space-y-2 sm:flex-row sm:flex-wrap sm:space-x-8 sm:space-y-0">
                     <li>
                         <a href="https://laravel.com/docs" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase" title="Documentation">Documentation</a>
                     </li>
