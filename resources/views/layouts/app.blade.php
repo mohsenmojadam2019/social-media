c7991fcd0c8aefc720c574aba7432674d7462f99
gech11 <getachewfikadu77@gmail.com>
2021-05-30 10:39:43 +0300

    initial commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,15 @@
                        Home
                     </a>
                     <a href="{{route('chat')}}" class="text-lg font-semibold text-gray-100 no-underline">chat</a>
-                    <span>notifications</span>
+                    <span class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z" />
+                          </svg>  
+                    </span>
+                    <span class="text-3xl">({{auth()->user()->notifications->count()}})</span>
+                    @foreach (auth()->user()->notifications as $notification)
+                      <li>{{$notification->data['data']}}</li>  
+                    @endforeach
                 </div>
                 <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
                     @guest
