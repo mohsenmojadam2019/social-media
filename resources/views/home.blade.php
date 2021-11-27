e11e1693e307a3eb81df9ad05d62991f67e6f87b
gech11 <getachewfikadu77@gmail.com>
2021-11-27 10:37:53 +0300

    initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,27 @@
 @extends('layouts.app')
 @section('content')
 <div>
-    @foreach ($posts as $post)
+  <div>
+   @foreach ($friends as $friend)
      <div>
-      <p>{{$post->title}}</p>
-      <img src="'posts'.{{$post->id}}" alt="">
-      <p>{{$post->body}}</p>
-      <like-component user-id="{{Auth::user()->id}}" post-id="{{$post->id}}"/>
-      <comment-component user-id="{{Auth::user()->id}}" post-id="{{$post->id}}"/>   
-    </div>        
-    @endforeach
+      <img src="/storage/users/profilepicture/{{$user->profile->avatar}}" >
+      <p>{{$user->name}}</p>  
+     </div>  
+   @endforeach
+  </div>
+  <div>
+    @foreach ($posts as $post)
+    <div>
+     <p>{{$post->title}}</p>
+     <img src="'posts'.{{$post->id}}" alt="">
+     <p>{{$post->body}}</p>
+     <like-component user-id="{{Auth::user()->id}}" post-id="{{$post->id}}"/>
+     <comment-component user-id="{{Auth::user()->id}}" post-id="{{$post->id}}"/>   
+   </div>        
+   @endforeach
+  </div>
+  <div>
+   
+  </div>
 </div>
 @endsection
