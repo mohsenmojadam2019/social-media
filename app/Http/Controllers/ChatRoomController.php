f30c8db7cca0ae019567f3a70671b48cb57a4bcd
gech11 <getachewfikadu77@gmail.com>
2021-11-29 03:24:32 +0300

    initial commit

---
+++ app/Http/Controllers/ChatRoomController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChatRoom;
+use App\Http\Requests\StoreChatRoomRequest;
+use App\Http\Requests\UpdateChatRoomRequest;
+
+class ChatRoomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreChatRoomRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreChatRoomRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ChatRoom  $chatRoom
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ChatRoom $chatRoom)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ChatRoom  $chatRoom
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ChatRoom $chatRoom)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateChatRoomRequest  $request
+     * @param  \App\Models\ChatRoom  $chatRoom
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateChatRoomRequest $request, ChatRoom $chatRoom)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ChatRoom  $chatRoom
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ChatRoom $chatRoom)
+    {
+        //
+    }
+}
