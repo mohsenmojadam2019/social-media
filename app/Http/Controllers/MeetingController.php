e6227ae89c149e0779c32b2ba08acb1a5996af6d
gech11 <getachewfikadu77@gmail.com>
2021-12-07 22:09:04 +0300

    meeting included

---
+++ app/Http/Controllers/MeetingController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Meeting;
+use App\Http\Requests\StoreMeetingRequest;
+use App\Http\Requests\UpdateMeetingRequest;
+
+class MeetingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreMeetingRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreMeetingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Meeting  $meeting
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Meeting $meeting)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Meeting  $meeting
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Meeting $meeting)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateMeetingRequest  $request
+     * @param  \App\Models\Meeting  $meeting
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateMeetingRequest $request, Meeting $meeting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Meeting  $meeting
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Meeting $meeting)
+    {
+        //
+    }
+}
