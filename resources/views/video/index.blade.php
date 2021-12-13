4c417565f2079f6098bedf3cf9f13f0c4f8452e0
gech11 <getachewfikadu77@gmail.com>
2021-12-13 22:52:27 +0300

    video pages created

--- resources/views/video/index.blade.php
+++ resources/views/video/index.blade.php
@@ -1,6 +1,23 @@
 @extends('layouts.app')
 @section('content')
  <div>
-  
+  @foreach ($videos as $video)
+   <div>
+     <div class="flex items-center">
+      <img src="/storage/videos/{{$video->channel->photo}}" class="w-10 h-10 rounded-full m-1"> 
+      <div>
+       <p>{{$video->channel->name}}</p>
+       <channel-follow-component :channel="{{$video->channel}}"/>
+        <p>{{$video->created-at}}</p>
+      </div>
+     </div>
+     <div>
+      <h1 class="text-xl text-bold">{{$video->title}}</h1>
+      <video src="/storage/videos/{{$video->source}}"></video>
+     </div>
+     <video-like-component/>
+     <video-comment-component/>
+   </div>   
+  @endforeach
  </div>
 @endsection
