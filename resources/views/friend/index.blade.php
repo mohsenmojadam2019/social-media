172088f1cd4fe6d77a19438ce6e709c648c0dd48
gech11 <getachewfikadu77@gmail.com>
2021-12-17 14:49:31 +0300

    friends page implemented

--- resources/views/friend/index.blade.php
+++ resources/views/friend/index.blade.php
@@ -1,27 +1,34 @@
 @extends('layouts.app')
 @section('content')
- <div>
-   <div class="flex m-2 p-2">
-     <h1 class="text-2xl font-bold">friend requests</h1>
-     @foreach ($frindrequests as $friend)
-      <div class="p-2 m-2 bg-white">
-      <img src="/storage/users/{{$user->avatar}}" class="w-32 rounded-xl">
-       <p class="text-xl text-bold">{{$friend->name}}</p>
-       <acceptrequest-component />
-       <removerequest-component />
-      </div>   
-     @endforeach
+ <div class="flex mt-3">
+   <div class="w-1/4 bg-blue-200">
+
    </div>
-   <div class="flexm-2 p-2">
-    <h1 class="text-2xl font-bold">people you may know</h1>
-    @foreach ($peoples as $person)
-     <div>
-     <img src="/storage/users/{{$person->avatar}}" class="w-32 rounded-xl">
-      <p>{{$person->name}}</p>
-      <addfriend-component />
-      <removefriend-component />
-     </div>   
-    @endforeach
-  </div>
+    <div class="w-1/2 bg-gray-200">
+     <h1 class="text-2xl font-bold text-center mb-2">people you may know</h1>
+     <div class="flex flex-wrap justify-center">
+      @foreach ($suggestedFriends as $friend)
+       <div>
+        <img src="https://cdn-media-1.freecodecamp.org/images/0*xkJgg-6HskYrQ3N7.jpeg" class="w-64 rounded-xl">
+        <p class="text-xl text-bold text-center">{{$friend->name}}</p>
+        <addfriend-component />
+        <removefriend-component />
+       </div>   
+      @endforeach
+     </div>
+    </div>
+    <div class="w-1/4 bg-green-200">
+     <h1 class="text-2xl font-bold text-center">friend requests({{$friendRequests->count()}})</h1>
+     <div class="flex flex-wrap">
+      @foreach ($friendRequests as $friend)
+       <div class="p-2 m-2">
+        <img src="https://cdn-media-1.freecodecamp.org/images/0*xkJgg-6HskYrQ3N7.jpeg" class="w-64">
+        <p class="text-xl text-bold text-center">{{$friend->name}}</p>
+        <acceptrequest-component />
+        <removerequest-component />
+      </div>   
+      @endforeach
+     </div>
+    </div>
  </div>
 @endsection
