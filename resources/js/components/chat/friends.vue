e12006fe464d6954d89f94c3b7a4bf5984a73906
gech11 <getachewfikadu77@gmail.com>
2022-01-17 15:13:59 -0500

    placeholder for chat photos implemented

--- resources/js/components/chat/friends.vue
+++ resources/js/components/chat/friends.vue
@@ -7,7 +7,8 @@
         <div @click="selectFriend(chatRoom.friend)" class="flex justify-between py-0.5 cursor-pointer px-2"
           :class="[chatRoom.friend.id==selectedFriend.id ? 'bg-first text-white hover:bg-first' : 'hover:bg-gray-200']">
          <div class="flex mx-2 space-y-2">
-           <img src="https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png" class="w-14 h-14 rounded-full mx-1">
+            <img v-if="chatRoom.friend.profile.avatar" src="https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png" class="w-14 h-14 rounded-full mx-1">
+            <div v-else class="w-14 h-14 rounded-full mx-1 text-white uppercase text-center text-2xl text-bold bg-first">{{chatRoom.friend.firstLetter}}</div>
            <div>
             <p v-if="chatRoom.friend.id==user.id" class="text-lg font-medium my-auto">saved messages</p>
             <p v-else class="text-lg font-medium my-auto" >{{chatRoom.friend.name}}</p>
@@ -46,6 +47,7 @@ export default {
         this.chatRooms=res.data.chatrooms;
         this.chatRooms.sort((room1,room2)=>(room1.lastMessage.created_at>room2.lastMessage.created_at)? -1 : 1);
         this.chatRooms.forEach(chatRoom=>{
+          chatRoom.friend.firstLetter=chatRoom.friend.name.substr(0,1);
           chatRoom.lastMessage.hour=chatRoom.lastMessage.created_at.substr(11,5);
           chatRoom.lastMessage.hour=(chatRoom.lastMessage.hour>12)? (parseInt(chatRoom.lastMessage.hour-12))+'PM' : chatRoom.lastMessage.hour+'AM';
         });
